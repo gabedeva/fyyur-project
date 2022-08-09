57031422138bbe80f06563f912285753f1390cd4
Gabriel Obafisile <obafisilegabriel@gmail.com>
2022-08-09 11:15:39 +0100

    edited config vars

--- projects/01_fyyur/starter_code/config.py
+++ projects/01_fyyur/starter_code/config.py
@@ -10,6 +10,5 @@ DEBUG = True
 
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@localhost:5432/fyyur'
-
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:<password>@localhost:<port>/<dbname>'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
